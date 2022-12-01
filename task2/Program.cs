324185f3c2c95eca4b2bd261f068c9289e42434d
Dmitriy <namek85@inbox.ru>
2022-12-01 23:45:52 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("input number 1");
+int N1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input number 2");
+int N2 = Convert.ToInt32(Console.ReadLine());
+
+if (N1 == N2 * N2)
+ {
+ Console.WriteLine("Yes");
+ }
+else{
+    Console.WriteLine("No");
+}
