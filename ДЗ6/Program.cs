056c1635f0cadf33ad457721035ab3a6b6930cbb
Дмитрий Иванов <namek85@inbox.ru>
2022-12-07 17:01:25 +0300

    ДЗ6

---
+++ ДЗ6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N%2 == 0)
+ {
+     Console.WriteLine("да");
+ }
+  else 
+ {
+    Console.WriteLine("нет");
+ }
