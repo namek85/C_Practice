a2bbbe09191bdc651125f6fac310b0c1e8545f3d
Дмитрий Иванов <namek85@inbox.ru>
2022-12-07 17:31:46 +0300

    ДЗ8

---
+++ ДЗ8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int I = 0;
+
+if (N%2 == 0)
+ {
+     
+ }
+  else 
+ {
+    N = N-1;
+ }
+
+while (I < N)
+ {
+    
+     Console.WriteLine(I+2);
+    I = I + 2;
+ }
+ 
