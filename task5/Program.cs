5628260119bba85fd9bf1d5b5bbbd356c5aad130
Дмитрий Иванов <namek85@inbox.ru>
2022-12-07 14:54:24 +0300

    task5

---
+++ task5/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Введите трёхзначное число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if ((N < 1000) && (N > 99))
+{ Console.WriteLine("Последняя цифра числа:" +N%10);}
+else 
+{Console.WriteLine("Неправильное число");
+}
