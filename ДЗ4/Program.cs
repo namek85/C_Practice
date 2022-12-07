27ebcd7947b2e606ec7320e4814e0f19867ae872
Дмитрий Иванов <namek85@inbox.ru>
2022-12-07 16:52:47 +0300

    ДЗ4

---
+++ ДЗ4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int P = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int K = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+ if (N > P)
+ {
+     max = N;
+ }
+  else 
+ {
+    max = P;
+ }
+
+ if (max > K)
+ { Console.WriteLine("max = " + max);}
+ else {Console.WriteLine ("max = " + K);}
