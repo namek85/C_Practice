6c792e07d804ec9e26e9867d9e421a751694c03a
Дмитрий Иванов <namek85@inbox.ru>
2022-12-07 15:47:00 +0300

    ДЗ1

---
+++ ДЗ1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int P = Convert.ToInt32(Console.ReadLine());
+ if (P > N)
+ {
+    Console.WriteLine("max = " + P);
+    }
+ else 
+ { 
+Console.WriteLine("max = " + N);}
